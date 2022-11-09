4a7186fe8e18e59f96127ae8296ba838797f14e6
Rahul7874 <rp158835@gmail.com>
2022-11-10 01:07:18 +0530

    day-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Cart from "./Pages/Cart"
 function App() {
   return (
     <div className="App">
-      
+      <Cart/>
     </div>
   );
 }
---
+++ frontend/src/Pages/Cart.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styles from "./Cart.module.css"
+const Cart = () => {
+  return (
+    <>
+    <div className={styles.navbar_cart}></div>   {/* import navbar at attch here */}
+
+    <div className={styles.basket_head}>
+        <h1>Your Shopping Basket</h1>
+        </div>
+     {/* product count div */}
+     <div className={styles.product_count}><span>1</span><span>Product</span></div> 
+     {/* all cart functionality div starts   */}
+     <div className={styles.cart_main}>
+        <div className={styles.items_div}></div>
+        {/* pin check div start */}
+        <div className={styles.price_main}>
+            <div className={styles.pin_check}>
+                <span>Deliver to</span>
+                <input/>
+                <button>Check</button>
+            </div>
+            {/* offer section start */}
+            <div className={styles.offer_div}>
+                <div className={styles.offer_img_div}><img src='https://www.lifestylestores.com/static/icons/gift-box.png' alt='offer'/></div>
+                <div>
+                    <p>Offers for you !</p>
+                    <p>Choose and apply voucher in 2 simple <br></br>
+                        steps</p>
+               </div>
+            </div>
+            {/* total pricing calculation div started */}
+            <div className={styles.price_calc}>
+                <div className={styles.mrp_div}>
+                    <div>Total MRP</div>
+                    <div>5999</div>
+                </div>
+                <div className={styles.discount_div}>
+                    <div>Offer discount</div>
+                    <div>-315</div>
+                </div>
+                <div className={styles.charge_div}>
+                    <div>Shipping charge</div>
+                    <div>Free</div>
+                </div>
+                <div className={styles.total_div}>
+                    <div>Total</div>
+                    <div>734</div>
+                </div>
+                <div className={styles.out_button}>
+                    <button>Checkout now</button>
+                </div>
+                <div>Shipping charges might vary based on pincode delivery <br></br>location</div>
+            </div>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default Cart
