ba880e9f214e4027b78dfc9ef6bdbfbc180b2126
HARIS#10# <mdharishz107@gmail.com>
2022-11-10 15:30:06 +0530

    womensHomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { SliderPage } from './Pages/Slider';
+import { WomenHomePage } from './Pages/WomenHomePage';
 
 function App() {
   return (
     <div className="App">
-      
+      <WomenHomePage/>
+      {/* <SliderPage/> */}
+    
     </div>
   );
 }
