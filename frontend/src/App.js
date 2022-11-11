c6910630b67c46cba64c0cb3444a5db0ce095ae2
HARIS#10# <mdharishz107@gmail.com>
2022-11-11 12:39:25 +0530

    added kids shop page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import WomensProducts from './Pages/Womens/WomensProducts';
+
+
 
 function App() {
   return (
     <div className="App">
-      <WomensProducts/>
+      
     </div>
   );
 }
