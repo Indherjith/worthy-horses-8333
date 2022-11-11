5197992039ab0183cb9f03d2da8f8791b96af0b4
HARIS#10# <mdharishz107@gmail.com>
2022-11-11 12:42:08 +0530

    just few updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import { SliderPage } from './Pages/Slider';
-import { WomenHomePage } from './Pages/WomenHomePage';
+
 
 function App() {
   return (
     <div className="App">
-      <WomenHomePage/>
-      {/* <SliderPage/> */}
+     
     
     </div>
   );
